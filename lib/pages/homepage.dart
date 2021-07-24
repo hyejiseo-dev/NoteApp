c13d309a7e6bc7803c06895761c699915ddf4ad7
hyejiseo-dev <dianna537@gmail.com>
2021-07-25 00:57:42 +0900

    homepage list complete

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase/firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mynote/pages/addnote.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,13 +15,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .collection('notes');
+
+  List<Color?> myColors = [
+    Colors.yellow[200],
+    Colors.red[200],
+    Colors.green[200],
+    Colors.deepPurple[200],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => AddNote()));
+                .push(
+              MaterialPageRoute(
+                builder: (context) => AddNote(),
+              ),
+            )
+                .then((value) {
+              print("Calling Set State!");
+              setState(() {});
+            });
           },
           child: Icon(
             Icons.add,
@@ -23,8 +49,66 @@ class _HomePageState extends State<HomePage> {
           ),
           backgroundColor: Colors.grey[700],
         ),
-        body: Center(
-          child: Text('Notes!'),
+        appBar: AppBar(
+          title: Text(
+            'Notes',
+            style: TextStyle(
+                fontSize: 24.0,
+                fontFamily: "lato",
+                fontWeight: FontWeight.bold,
+                color: Colors.white60),
+          ),
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+        ),
+        body: FutureBuilder<QuerySnapshot>(
+          future: ref.get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    Random random = new Random();
+                    Color? bg = myColors[random.nextInt(4)];
+                    Map data = snapshot.data!.docs[index].data() as Map;
+                    DateTime mydateTime = data['created'].toDate();
+                    return Card(
+                      color: bg,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${data['title']}',
+                                style: TextStyle(
+                                    fontSize: 24.0,
+                                    fontFamily: "lato",
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87),
+                              ),
+                              Container(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  DateFormat.yMMMd()
+                                      .add_jm()
+                                      .format(mydateTime),
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontFamily: "lato",
+                                      color: Colors.black87),
+                                ),
+                              )
+                            ]),
+                      ),
+                    );
+                  });
+            } else {
+              return Center(
+                child: Text('Loading...'),
+              );
+            }
+          },
         ));
   }
 }
